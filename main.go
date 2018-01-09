c9f9f6a1386550302be00094e9b0ceb651751c49
Suyash Kumar <suykumar@05088a-suykumar.corp.zynga.com>
2018-01-09 18:26:44 +0530

    can be improved

--- main.go
+++ main.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
+var fileName string
+
 func main() {
-	PORT := "127.0.0.1:8060"
 
-	log.Print("Running server on " + PORT)
+	var PORT string
 
-	http.HandleFunc("/", simpleFunc)
-	http.HandleFunc("/static/", staticFunc)
+	if len(os.Args) <= 1 {
+		log.Fatal("Correct syntax - ./main <fileName> [<IP Address:PORT>]")
+		os.Exit(1)
+	} else {
+		fileName = os.Args[1]
+
+		if len(os.Args) > 2 {
+			PORT = os.Args[2]
+		} else {
+			PORT = ":8060"
+		}
+	}
+
+	log.Println("Running server on " + PORT)
+	log.Println("Requested file - " + fileName)
+
+	http.HandleFunc("/", staticFunc)
 
 	log.Fatal(http.ListenAndServe(PORT, nil))
 }
 
-func simpleFunc(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
-}
+func staticFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
 
-func staticFunc()
+		// if file is present
+
+		if data, err := ioutil.ReadFile(fileName); err == nil {
+			w.Write(data)
+		} else {
+			w.WriteHeader(404)
+			w.Write([]byte("The requested file does not exist"))
+		}
+	}
+}
